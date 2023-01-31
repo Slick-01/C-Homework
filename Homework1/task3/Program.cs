1d49a329f37350dec6e5c298ce45a736d5831c4a
Falaleeva <anna.falaleeva@sap.com>
2023-01-31 17:48:53 +0000

    task3 and 6

---
+++ Homework1/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+        Console.WriteLine("Put 3 numbers");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        int max = a;
+
+        if (b > a && b > c) max = b;
+            else if (c > a && c > b)
+        {
+            max = c;
+        }
+        Console.WriteLine("max" + " = " + max);
+            
---
+++ Homework1/task6/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Put number");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) Console.WriteLine(a + " " + "even number"); 
+else {
+    Console.WriteLine(a + " " + "odd number");
+}
