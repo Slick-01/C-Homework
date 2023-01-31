64e2750e7205703c87314a8ed4700517f4d36ebb
Falaleeva <anna.falaleeva@sap.com>
2023-01-31 19:37:00 +0000

    task10

---
+++ Homework2/task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Put number");
+
+int a = Convert.ToInt32(Console.ReadLine());
+string TwoNumber = Convert.ToString(a);
+
+System.Console.WriteLine("Second number is" + " " + TwoNumber[1]);
+
+
