e0a37996bafb11f049a7a6c14e8e93bc4e278b74
Falaleeva <anna.falaleeva@sap.com>
2023-01-31 19:37:56 +0000

    add task 13 and 15

---
+++ Homework2/task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Put number");
+
+int a = Convert.ToInt32(Console.ReadLine());
+string ThirdNumber = Convert.ToString(a);
+
+if (ThirdNumber.Length > 2)
+
+System.Console.WriteLine("Third number is" + " " + ThirdNumber[2]);
+
+else {
+  System.Console.WriteLine("Third number is not");  
+}
---
+++ Homework2/task15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Put number");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 6 || a == 7) 
+    System.Console.WriteLine("This day is weekend");
+
+else if (a >= 1 && a <=5){
+  System.Console.WriteLine("This day is workday");  
+}
+
+else {
+   System.Console.WriteLine("This number is not a day in the week"); 
+}
