8cddfe85d4c3520d7833cd03349df847f03ba2f6
Falaleeva <anna.falaleeva@sap.com>
2023-02-01 17:11:35 +0000

    add task 19, 21 and 23

---
+++ Homework3/task19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int rnd = new Random().Next(10001, 100000);
+// Это для проверки конкретного палиндромного числа
+//Console.WriteLine("Put number");
+//int rnd = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(rnd);
+
+int num = (rnd % 10 )* 10000 + ((rnd / 10) % 10) * 1000 + ((rnd / 100) % 10) * 100 + ((rnd / 1000 ) % 10) * 10 + rnd / 10000;
+
+if (rnd == num)
+    System.Console.WriteLine("Number is palindrom" +  " " + num);
+else {
+    System.Console.WriteLine("Number is not palindrom" +  " " + rnd);
+}
---
+++ Homework3/task23/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int rnd = new Random().Next(-11, 11);
+System.Console.WriteLine(rnd);
+
+for (int i = 1; i <= rnd; i ++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}
