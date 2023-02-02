dfbe2cfc2f5ba2693143c8ea099e430db805c546
Falaleeva <anna.falaleeva@sap.com>
2023-02-02 21:52:01 +0000

    Add task 34, 36 and 38

---
+++ Homework5/task36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine(GetEvenNumber(arr));
+
+int GetEvenNumber(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+            sum += arr[i];
+    }
+    return sum;
+}
+
+int[] GetRandomArray(int length){
+    int[] result = new int[length];
+
+    for(int i = 0; i < length; i++){
+        result[i] = new Random().Next(0, 100);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int j in arr)
+    {
+        Console.Write(j + " ");
+    }
+    System.Console.WriteLine();
+}
---
+++ Homework5/task38/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine(GetMin(arr));
+System.Console.WriteLine(GetMax(arr));
+
+int GetMin(int[] array)
+{
+   int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+int GetMax(int[] array)
+{
+   
+   int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int[] GetRandomArray(int length){
+    int[] result = new int[length];
+
+    for(int i = 0; i < length; i++){
+        result[i] = new Random().Next(0, 100);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int j in arr)
+    {
+        Console.Write(j + " ");
+    }
+    System.Console.WriteLine();
+}
+
